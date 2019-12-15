aa0d2636e85f1845b3f994e5225827ef54db634f
Luis Miguel Ballestas <luismballestas@gmail.com>
2019-12-15 17:37:16 -0500

    Initial commit

---
+++ app/Providers/SparkServiceProvider.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Spark\Spark;
+use Laravel\Spark\Providers\AppServiceProvider as ServiceProvider;
+use Carbon\Carbon;
+
+class SparkServiceProvider extends ServiceProvider
+{
+    /**
+     * Your application and company details.
+     *
+     * @var array
+     */
+    protected $details = [
+        'vendor' => 'Your Company',
+        'product' => 'Your Product',
+        'street' => 'PO Box 111',
+        'location' => 'Your Town, NY 12345',
+        'phone' => '555-555-5555',
+    ];
+
+    /**
+     * The address where customer support e-mails should be sent.
+     *
+     * @var string
+     */
+    protected $sendSupportEmailsTo = null;
+
+    /**
+     * All of the application developer e-mail addresses.
+     *
+     * @var array
+     */
+    protected $developers = [
+        //
+        'luismballestas@gmail.com'
+    ];
+
+    /**
+     * Indicates if the application will expose an API.
+     *
+     * @var bool
+     */
+    protected $usesApi = true;
+
+    /**
+     * Finish configuring Spark for the application.
+     *
+     * @return void
+     */
+    public function booted()
+    {
+        Spark::noCardUpFront()->trialDays(14);
+
+        Spark::freePlan()
+            ->features([
+                'First', 'Second', 'Third'
+            ]);
+
+        Spark::plan('Basic', 'provider-id-1')
+            ->price(10)
+            ->features([
+                'First', 'Second', 'Third'
+            ]);
+
+        Spark::validateUsersWith(function () {
+            return [
+                'name' => 'required|max:255',
+                'company' => 'required|min:3|max:255',
+                'email' => 'required|email|max:255|unique:users',
+                'password' => 'required|confirmed|min:6',
+                'terms' => 'required|accepted',
+            ];
+        });
+        //Alternative to closure:
+        //Spark::validateUsersWith('UserValidator@validate');
+
+        Spark::createUsersWith(function ($request) {
+            $user = Spark::user();
+
+            $data = $request->all();
+
+            $user->forceFill([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'company' => $data['company'],
+                'password' => bcrypt($data['password']),
+                'last_read_announcements_at' => Carbon::now(),
+                'trial_ends_at' => Carbon::now()->addDays(Spark::trialDays()),
+            ])->save();
+
+            return $user;
+        });
+        //Alternative to closure:
+        //Spark::createUsersWith('UserCreator@create');
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        parent::register();
+    }
+}
